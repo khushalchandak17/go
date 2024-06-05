be6563f22f9ba21b8d7e76e6fc545318c3e1526d
khushalchandak17 <khushal.chandak@suse.com>
2024-06-06 01:17:23 +0530

    pointer

---
+++ mycodes/07mypointers/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to world of pointers")
+
+	//  var one int
+	// var ptr *int
+	// var ptrs *string
+	// fmt.Println("Value of pointer is ", ptr)
+
+	myNumber := 23
+	var ptr = &myNumber
+
+	// Refrence means &... simple
+	// Note in "var ptr *int" we created only pointer, but above we are creating and also refrenceing it at the same time
+
+	fmt.Println("Value of actual pointer is ", ptr)
+
+	fmt.Println("Value in side pointer is ", *ptr)
+	fmt.Println("Value in side pointer in other way ", *&myNumber)
+
+	// If you need the value inside pointer address simple add * in it
+
+	*ptr = *ptr + 2
+	fmt.Println("New value is ", myNumber)
+}
