dbca5b22bda567d1736b3df9b9df31722a557b80
khushalchandak17 <khushal.chandak@suse.com>
2024-06-06 12:43:23 +0530

    webrequest

---
+++ mycodes/19webrequest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://www.google.com"
+
+func main() {
+	fmt.Println("Hi welcome to WebRequest Session")
+
+	// https://pkg.go.dev/net/http#Response
+
+	// Close records whether the header directed that the connection be
+	// closed after reading Body. The value is advice for clients: neither
+	// ReadResponse nor Response.Write ever closes a connection.
+
+	//   Close bool
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response is of type %T\n", response)
+	fmt.Println(response)
+	defer response.Body.Close() // callers responsibility to close the connection
+
+	databyte, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databyte)
+	fmt.Println(content)
+
+}
